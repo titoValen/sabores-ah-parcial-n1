bac3fb2b8bda387d7a0d3cf5c17b25aaa408a346
titoValen <valentin.tito@davinci.edu.ar>
2026-09-23 16:38:08 -0300

    feat: Añadir configuración de conexión a MongoDB en db.js

---
+++ config/db.js
@@ -0,0 +1,24 @@
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const DB_NAME = process.env.DB_NAME;
+
+const client = new MongoClient(MONGO_URI);
+
+let db;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db(DB_NAME);
+    console.log("Conexión a la base de datos establecida - " + DB_NAME);
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+    process.exit(1);
+  }
+}
+
+export { connectDB, db, ObjectId };
