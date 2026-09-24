11c00c5e2e13cb6601d09caea373b56994691d1d
titoValen <valentin.tito@davinci.edu.ar>
2026-09-24 02:28:58 -0300

    feat: Añadir ruta para obtener platos de un chef específico

--- api/routes/chefs.route.js
+++ api/routes/chefs.route.js
@@ -4,6 +4,7 @@ import * as chefsController from "../controllers/chefs.controller.js";
 const router = Router();
 
 router.get("/", chefsController.getChefs);
+router.get("/:id/dishes", chefsController.getChefDishes);
 router.get("/:id", chefsController.getChefById);
 router.post("/", chefsController.createChef);
 router.put("/:id", chefsController.updateChef);
