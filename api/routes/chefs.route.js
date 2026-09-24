67fb1dd071465a1d93939e4e518a700e7e3cd6ca
titoValen <valentin.tito@davinci.edu.ar>
2026-09-24 01:47:40 -0300

    feat: Añadir rutas para la gestión de chefs en chefs.route.js

---
+++ api/routes/chefs.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as chefsController from "../controllers/chefs.controller.js";
+
+const router = Router();
+
+router.get("/", chefsController.getChefs);
+router.get("/:id", chefsController.getChefById);
+router.post("/", chefsController.createChef);
+router.put("/:id", chefsController.updateChef);
+router.delete("/:id", chefsController.deleteChef);
+
+export default router;
