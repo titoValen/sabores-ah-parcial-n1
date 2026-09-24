e980c1a945b9886c2e582cca8eaae2b0209fd754
titoValen <valentin.tito@davinci.edu.ar>
2026-09-24 02:29:56 -0300

    feat: Refactorizar la configuración del puerto para usar variables de entorno

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import dishesRoutes from "./api/routes/dishes.route.js";
 import chefsRoutes from "./api/routes/chefs.route.js";
 
 const app = express();
+const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use("/api/dishes", dishesRoutes);
@@ -12,7 +13,7 @@ app.use("/api/chefs", chefsRoutes);
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(3333, () => console.log("Servidor corriendo en el puerto 3333"));
+    app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
   } catch (error) {
     console.error("Error al iniciar el servidor:", error);
     process.exit(1);
