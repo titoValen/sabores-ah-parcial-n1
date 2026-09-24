6faf7b206db8023f14c49c0ef44b7a6b5c536f3a
titoValen <valentin.tito@davinci.edu.ar>
2026-09-24 15:44:29 -0300

    feat: Añadir rutas para renderizar la lista de chefs y el formulario de nuevo chef

--- routes/view.route.js
+++ routes/view.route.js
@@ -8,5 +8,7 @@ router.get("/sections/:slug", viewController.renderSection);
 router.get("/dishes/new", viewController.renderNewDish);
 router.get("/dishes/:id/edit", viewController.renderEditDish);
 router.get("/dishes/:id", viewController.renderDish);
+router.get("/chefs", viewController.renderChefs);
+router.get("/chefs/new", viewController.renderNewChef);
 
 export default router;
